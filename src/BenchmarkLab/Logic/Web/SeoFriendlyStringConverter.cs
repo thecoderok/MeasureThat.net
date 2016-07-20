a1db36e4c31d97dc52673dedf958a27a5c5e3fb8
Vitalii Ganzha <vitaliiy@outlook.com>
2016-07-20 15:26:45 -0700

    added class for seo friendly url generation

---
+++ src/BenchmarkLab/Logic/Web/SeoFriendlyStringConverter.cs
@@ -0,0 +1,14 @@
+using JetBrains.Annotations;
+using System;
+
+namespace BenchmarkLab.Logic.Web
+{
+    public class SeoFriendlyStringConverter
+    {
+        // http://www.jerriepelser.com/blog/generate-seo-friendly-urls-aspnet-mvc
+        public static string Convert([NotNull] string input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
