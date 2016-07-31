8a0dad6f012fbc67ca473d84d2d19353e1ca8a01
Vitalii Ganzha <vitaliiy@outlook.com>
2016-07-31 15:58:10 -0700

    tables for results, added ua parser from https://github.com/tobie/ua-parser Issues: #12, #13, #14

---
+++ BenchmarkLab.DB/Tables/ResultRow.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[ResultRow]
+(
+	[Id] BIGINT NOT NULL PRIMARY KEY, 
+    [ResultId] BIGINT NOT NULL, 
+    [ExecutionsPerSecond] REAL NOT NULL, 
+    [RelativeMarginOfError] REAL NOT NULL, 
+    [NumberOfSamples] INT NOT NULL, 
+    [TestName] NVARCHAR(50) NOT NULL, 
+    CONSTRAINT [FK_ResultRow_ToResult] FOREIGN KEY ([ResultId]) REFERENCES [Result]([Id])
+)
---
+++ BenchmarkLab.DB/Tables/Results.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Result]
+(
+	[Id] BIGINT NOT NULL PRIMARY KEY, 
+    [BenchmarkId] BIGINT NOT NULL, 
+    [RawUAString] NVARCHAR(100) NOT NULL, 
+    [Browser] NVARCHAR(50) NOT NULL, 
+    [Created] DATETIME2 NOT NULL DEFAULT getdate(), 
+    [UserId] NVARCHAR(450) NULL, 
+    [DevicePlatform] NVARCHAR(50) NOT NULL, 
+    CONSTRAINT [FK_Results_ToBenchmark] FOREIGN KEY ([BenchmarkId]) REFERENCES [Benchmark]([Id]), 
+    CONSTRAINT [FK_Result_ToUsers] FOREIGN KEY ([UserId]) REFERENCES [AspNetUsers]([Id])
+)
