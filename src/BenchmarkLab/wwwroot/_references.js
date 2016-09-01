c592adc274df6cde23da9ebd74ae3450b49befe2
Vitalii Ganzha <vitaliiy@outlook.com>
2016-08-31 23:09:57 -0700

    new link to featured benchmark

--- src/BenchmarkLab/wwwroot/_references.js
+++ src/BenchmarkLab/wwwroot/_references.js
@@ -11,5 +11,3 @@
 /// <reference path="lib/jquery-validation-unobtrusive/jquery.validate.unobtrusive.js" />
 /// <reference path="lib/mustache/mustache.js" />
 /// <reference path="lib/platform/platform.js" />
-/// <reference path="scripts/admin-app.js" />
-/// <reference path="scripts/benchmarklab.js" />
