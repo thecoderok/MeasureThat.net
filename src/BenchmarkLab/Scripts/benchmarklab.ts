e11507e2583be951e40126269a1236454665c7c3
Vitalii Ganzha <vitaliiy@outlook.com>
2016-09-07 17:29:35 -0700

    started porting raw embedded js to typescript

--- src/BenchmarkLab/Scripts/benchmarklab.ts
+++ src/BenchmarkLab/Scripts/benchmarklab.ts
@@ -1,5 +1,49 @@
-﻿function sayHello() {
-    const compiler = (document.getElementById("compiler") as HTMLInputElement).value;
-    const framework = (document.getElementById("framework") as HTMLInputElement).value;
-    return `Hello from ${compiler} and ${framework}!`;
+﻿/// <reference path="../typings/globals/jquery/index.d.ts" />
+/// <reference path="../typings/globals/codemirror/index.d.ts" />
+
+class AddNewTestPageController {
+    testCaseCounter: number;
+
+    constructor() {
+        $(document).ready(() => this.initialize());
+    }
+
+    // Deletes test case input panel from the page
+    deleteTest(testCase: HTMLElement): boolean {
+        $(testCase.parentNode.parentNode).remove();
+        return false;
+    }
+
+    // Creates new test case input panel
+    makeNewTestCase(strTestCaseContent: string, strTestCaseName: string): void {
+        
+    }
+
+    // Initialize controller
+    initialize(): void {
+        var editor = CodeMirror.fromTextArea(document.getElementById("HtmlPreparationCode") as HTMLTextAreaElement, {
+            lineNumbers: true,
+            mode: "xml",
+            value: "\n\n\n"
+        });
+        editor.on("blur", instance => {
+            (instance as any).save();
+        });
+
+        editor = CodeMirror.fromTextArea(document.getElementById("ScriptPreparationCode") as HTMLTextAreaElement, {
+            lineNumbers: true,
+            mode: 'javascript'
+        });
+        editor.on("blur", instance => {
+            (instance as any).save();
+        });
+        //editor.setValue(' \n \n \n');
+
+        $("[data-action='new-test']").on("click", () => this.makeNewTestCase("", ""));
+
+        // Preserve tests previously entered by user
+        $("[data-content='existing-test']").each((idx, el) => {
+            this.makeNewTestCase(el.textContent, $(el).attr("data-test-name"));
+        });
+    }
 }
