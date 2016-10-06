d68f29c58f1dfd37732630f28ba532cf9566d8d1
Vitalii Ganzha <vitaliiy@outlook.com>
2016-10-06 09:51:02 -0700

    working angular component

--- src/BenchmarkLab/Scripts/frontendapp.ts
+++ src/BenchmarkLab/Scripts/frontendapp.ts
@@ -1,22 +1,47 @@
-var personApp = angular.module('benchmarkApp', []);
-personApp.controller('benchmarksController', ['$scope', $scope => {
-    $scope.firstName = "Mary";
-    $scope.lastName = "Jane";
-}]);
+var appId: string = "benchmarkApp";
+var factoryId: string = 'benchmarkListFactory';
 
+(() => {
+    'use strict';
+    var app = angular.module(appId, []);
+    app.factory(factoryId, ['$http', benchmarkListFactory]);
 
-// controller
-var BenchmarksController = function () {
+    function benchmarkListFactory($http: ng.IHttpService) {
+        function getBenchmarks(): ng.IHttpPromise<any>{
+            return $http.get('/api/Benchmarks');
+        }
 
-    var vm = this;
-    vm.firstName = "Aftab";
-    vm.lastName = "Ansari";
-}
+        const service = {
+            getBenchmarks: getBenchmarks
+        };
 
-// component
-personApp.component('benchmarkList', {
-    templateUrl: '/js/partials/benchmarkList.html',
-    controller: BenchmarksController,
-    controllerAs: 'vm'
+        return service;
+    }
 
-});
+    // controller
+    var BenchmarksController = function (benchmarkListFactory) {
+
+        var vm = this;
+        vm.benchmarks = [];
+
+        benchmarkListFactory.getBenchmarks()
+            .success(data => {
+                this.benchmarks = data;
+                console.log('Received benchmarks: ' + data.length);
+            })
+            .error(error => {
+                alert(error);
+            });
+    }
+
+    // component
+    app.component('benchmarkList', {
+        templateUrl: '/js/partials/benchmarkList.html',
+        controller: BenchmarksController,
+        controllerAs: 'vm',
+        bindings: {
+            benchmarks: '@'
+        }
+    });
+})();
+
