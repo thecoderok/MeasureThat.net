e2c075ac8206630e47049767df6cdb61f2dad029
Vitalii Ganzha <vitaliiy@outlook.com>
2019-09-04 06:40:47 -0700

    added unit tests project

---
+++ tests/MeasureThat.Net.Tests/BenchmarksControllerTests.cs
@@ -0,0 +1,14 @@
+using MeasureThat.Net.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MeasureThat.Net.Tests
+{
+    [TestClass]
+    public class BenchmarksControllerTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
