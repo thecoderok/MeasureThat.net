9bb269769de8ef078060ec1b19aac10a55acd28b
vmganzha <vitaliiy@outlook.com>
2020-04-29 18:33:09 -0700

    more host & ip tools

--- src/BenchmarkLab/Controllers/ToolsController.cs
+++ src/BenchmarkLab/Controllers/ToolsController.cs
@@ -2,7 +2,7 @@
 using System.Net;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Whois;
+using Whois.NET;
 
 namespace BenchmarkLab.Controllers
 {
@@ -68,9 +68,8 @@ namespace BenchmarkLab.Controllers
 
             try
             {
-                var whois = new WhoisLookup();
-                var response = await whois.LookupAsync(domain);
-                return View(response);
+                var result = await WhoisClient.QueryAsync(domain);
+                return View(result);
             } catch (Exception e)
             {
                 ViewData["error"] = e.Message;
@@ -100,14 +99,36 @@ namespace BenchmarkLab.Controllers
 
         public async Task<IActionResult> GetIPAddressesByHostName(string host)
         {
+            ViewData["host"] = host;
             if (String.IsNullOrWhiteSpace(host))
             {
                 return View();
             }
+            
             try
-            {
+            { 
                 IPAddress[] addresses = await Dns.GetHostAddressesAsync(host);
                 return View(addresses);
+            }
+            catch (Exception e)
+            {
+                ViewData["error"] = e.Message;
+                return View();
+            }
+        }
+
+        public async Task<IActionResult> GetHostsByIPAddress(string ip)
+        {
+            ViewData["ip"] = ip;
+            if (String.IsNullOrWhiteSpace(ip))
+            {
+                return View();
+            }
+
+            try
+            {
+                IPHostEntry entries = await Dns.GetHostEntryAsync(ip);
+                return View(entries);
             }
             catch (Exception e)
             {
