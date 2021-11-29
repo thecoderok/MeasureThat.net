25c8a24d98a17a2049916d0b307b35b8f3db73a0
Vitalii Ganzha <vitaliiy@outlook.com>
2021-11-28 16:15:37 -0800

    started working on RunThat feature

---
+++ src/BenchmarkLab/Controllers/RunThatController.cs
@@ -0,0 +1,49 @@
+﻿
+using System.Threading.Tasks;
+using MeasureThat.Net.Logic.Web;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BenchmarkLab.Controllers
+{   
+    // Run JS with HTML & CSS from the browser
+    public class RunThatController : Controller
+    {
+        public IActionResult Index()
+        {
+            // View with explanation of the feature
+            // Show user's workspaces
+            // Should workspace only be visible to creator?
+            // Or they all should be public?
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [ServiceFilter(typeof(ValidateReCaptchaAttribute))]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        public IActionResult Show(long id)
+        {
+            // Show js/html/css and iframe with results
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Delete(long id)
+        {
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [ServiceFilter(typeof(ValidateReCaptchaAttribute))]
+        public async Task<IActionResult> Edit(long id)
+        {
+            return RedirectToAction("Index");
+        }
+    }
+}
