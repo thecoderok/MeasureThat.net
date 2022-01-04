197d9bfff56aad4a80a696765ebbc2bf7abb9f6a
Vitalii Ganzha <vitaliiy@outlook.com>
2022-01-03 17:27:28 -0800

    savethat

---
+++ BenchmarkLab.DB/Tables/SaveThatBlob.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[SaveThatBlob]
+(
+    [Id] INT NOT NULL PRIMARY KEY,
+    [Name] NVARCHAR(200) NOT NULL, 
+    [OwnerId] nvarchar(450) NOT NULL,
+    [WhenCreated] DATETIME2 NOT NULL , 
+    [Blob] NVARCHAR(MAX) NOT NULL, 
+    [Language]  NVARCHAR(40) NULL,
+    CONSTRAINT [FK_SaveThatBlob_ToUsers] FOREIGN KEY (OwnerId) REFERENCES [AspNetUsers]([Id])
+)
