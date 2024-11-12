36db561230afd1030b10ba41c82b6e717e6db64f
thecoderok <thecoderok@users.noreply.github.com>
2024-11-12 08:56:39 -0800

    more auth tests, getting rid of auth modal since it just adds one more hop to the actual login page

--- E2ETests/CreateEditForkDeleteBenchmarkTest.cs
+++ E2ETests/CreateEditForkDeleteBenchmarkTest.cs
@@ -1,10 +1,23 @@
-﻿using Microsoft.Playwright;
-
-namespace E2ETests
+﻿namespace E2ETests
 {
     [TestClass]
     public class CreateEditForkDeleteBenchmarkTest : BenchmarkLabBaseTest
     {
+        // TODO: how to avoid relative path here? 
+        const string TEST_CONFIG_FILE = "../../../TestConfig.ini";
+
+        private struct Credentials
+        {
+            public string Username
+            {
+                get; set;
+            }
+            public string Password
+            {
+                get; set;
+            }
+        }
+
         private Dictionary<string, string> ReadCredentials(string filePath)
         {
             var credentials = new Dictionary<string, string>();
@@ -23,32 +36,71 @@ namespace E2ETests
             return credentials;
         }
 
-        [TestMethod]
-        public async Task TestAccountLogin()
+        private Credentials GetCredentials(string filePath)
         {
-            var credentials = ReadCredentials("../../../TestConfig.ini");
-            var username = credentials["username"];
-            var password = credentials["password"];
+            var credentialsDict = ReadCredentials(filePath);
+            var username = credentialsDict["username"];
+            var password = credentialsDict["password"];
 
             if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
             {
                 throw new InvalidOperationException("Test credentials are not set in TestConfig.ini.");
             }
 
+            return new Credentials { Username = username, Password = password };
+        }
+
+        [TestMethod]
+        public async Task TestAccountLogin()
+        {
+            var credentials = GetCredentials(TEST_CONFIG_FILE);
+
             // Navigate to the login page
             await Page.GotoAsync("/Account/Login");
 
-            // Fill in the login form
-            await Page.FillAsync("#Email", username);
-            await Page.FillAsync("#Password", password);
-            await Page.ClickAsync("button[type='submit'].btn.btn-default");
-            await Expect(Page).ToHaveTitleAsync(new Regex("Home Page - MeasureThat.net"));
+            await LoginAsync(credentials);
 
-            var userElement = Page.Locator($"a[title='Manage'][href='/Manage']:has-text('Hello {username}!')");
+            var userElement = Page.Locator($"a[title='Manage'][href='/Manage']:has-text('Hello {credentials.Username}!')");
             await Expect(userElement).ToBeVisibleAsync();
 
             var logOffButton = Page.Locator("button[type='submit'].btn.btn-link.navbar-btn.navbar-link");
             await Expect(logOffButton).ToBeVisibleAsync();
+            await logOffButton.ClickAsync();
+            await Expect(userElement).Not.ToBeVisibleAsync();
+
+            var registerLink = Page.Locator("a[href='/Account/Register']:has-text('Register')");
+            await Expect(registerLink).ToBeVisibleAsync();
+
+            var loginLink = Page.Locator("a[href='/Account/Login']:has-text('Log in')");
+            await Expect(loginLink).ToBeVisibleAsync();
+        }
+
+        [TestMethod]
+        public async Task TestCreateEditForkDeleteBenchmark()
+        {
+            // Login via main page/modal dialog
+            await Page.GotoAsync("/");
+
+            var credentials = GetCredentials(TEST_CONFIG_FILE);
+            var loginLink = Page.Locator("a[href='/Account/Login']:has-text('Log in')");
+
+            await loginLink.ClickAsync();
+
+            await LoginAsync(credentials);
+
+            var userElement = Page.Locator($"a[title='Manage'][href='/Manage']:has-text('Hello {credentials.Username}!')");
+            await Expect(userElement).ToBeVisibleAsync();
+
+            // Click Create new test
+            var createBenchmarkLink = Page.Locator("div.jumbotron a.btn.btn-primary[href='/Benchmarks/Add']");
+        }
+
+        private async Task LoginAsync(Credentials credentials)
+        {
+            await Page.FillAsync("#Email", credentials.Username);
+            await Page.FillAsync("#Password", credentials.Password);
+            await Page.ClickAsync("button[type='submit'].btn.btn-default");
+            await Expect(Page).ToHaveTitleAsync(new Regex("Home Page - MeasureThat.net"));
         }
     }
 }
