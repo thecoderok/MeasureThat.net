8c4f4769778d4fc9db270e3fcf6f52ae856c53c8
thecoderok <thecoderok@users.noreply.github.com>
2025-03-26 19:49:27 -0700

    fix "Forgot password link does not work - emails are not being sent #91

--- src/BenchmarkLab/Services/MessageServices.cs
+++ src/BenchmarkLab/Services/MessageServices.cs
@@ -1,6 +1,7 @@
 using Microsoft.Extensions.Options;
 using SendGrid;
 using SendGrid.Helpers.Mail;
+using System;
 using System.Threading.Tasks;
 
 namespace MeasureThat.Net.Services
@@ -20,19 +21,22 @@ namespace MeasureThat.Net.Services
             get;
         } //set only via Secret Manager
 
-        // https://azure.microsoft.com/en-us/documentation/articles/sendgrid-dotnet-how-to-send-email/#create-a-sendgrid-account
-        public Task SendEmailAsync(string email, string subject, string message)
+        // https://github.com/sendgrid/sendgrid-csharp
+        public async Task SendEmailAsync(string email, string subject, string message)
         {
-            var client = new SendGridClient(Options.SendGridApiKey);
-
-            // Send a Single Email using the Mail Helper
+            var apiKey = Options.SendGridApiKey;
+            var client = new SendGridClient(apiKey);
             var from = new EmailAddress(Options.SenderEmail, Options.SenderName);
             var to = new EmailAddress(email);
             var plainTextContent = message;
             var htmlContent = message;
             var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
-
-            return client.SendEmailAsync(msg);
+            var response = await client.SendEmailAsync(msg);
+            if (!response.IsSuccessStatusCode)
+            {
+                var responseBody = await response.Body.ReadAsStringAsync();
+                throw new Exception($"Unable to send an email: {response.StatusCode} - {responseBody}");
+            }
         }
 
         public Task SendSmsAsync(string number, string message)
