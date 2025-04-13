18185ba80b5037dd1f8fba7c5fcd332861beb17a
vmg.registrations <vmg.registrations@outlook.com>
2025-04-13 12:36:06 -0700

    Update EF Core model and migration files, to migrate to EF 9
    Significant updates to the Entity Framework Core model for the `BenchmarkLab` application, including modifications to `ApplicationDbContextModelSnapshot.cs` to reflect new entity configurations, property types, and lengths. Added new properties like `RelatedBenchmarks` and `WhenUpdated`, renamed primary keys, and created new indexes for improved performance.
    
    The migration file `20250413193228_some_missing_migration.cs` includes operations to drop existing primary keys, rename indexes, alter column types, and add new columns, along with the creation of a new `GenAIDescription` table. The `Down` method is implemented for rollback capabilities.
    
    Updated `20250413193228_some_missing_migration.Designer.cs` to ensure the generated model aligns with the new database schema.

--- src/BenchmarkLab/Migrations/20250413193228_some_missing_migration.Designer.cs
+++ src/BenchmarkLab/Migrations/20250413193228_some_missing_migration.Designer.cs
@@ -4,6 +4,7 @@ using MeasureThat.Net.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BenchmarkLab.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250413193228_some_missing_migration")]
+    partial class some_missing_migration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
